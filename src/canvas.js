97142fdedc0a8eb4feccfda9e39c1d1b18a2b569
christopher4lis <christopher4lis@gmail.com>
2017-10-02 12:22:36 -0400

    Add distance formula to utilities and add eslint.

--- src/canvas.js
+++ src/canvas.js
@@ -43,6 +43,13 @@ function randomColor(colors) {
 	return colors[Math.floor(Math.random() * colors.length)];
 }
 
+function distance(x1, y1, x2, y2) {
+    const xDist = x2 - x1;
+    const yDist = y2 - y1;
+
+    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+}
+
 
 // Objects
 function Object(x, y, radius, color) {
