ec5a0727ccf6bc13dc98fb143b6af87ef9e5107b
christopher4lis <christopher4lis@gmail.com>
2019-02-04 15:56:16 -0500

    Hide browsersync notification.

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,39 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/canvas.js',
-    output: {
-        path: __dirname + '/dist/',
-        filename: './js/canvas.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new BrowserSyncPlugin({
-            host: 'localhost',
-            port: 3000,
-            server: { baseDir: ['dist'] },
-            files: ['./dist/*']
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/index.html'
-        })
-    ],
-    watch: true,
-    devtool: 'source-map'
+  mode: 'development',
+  entry: './src/canvas.js',
+  output: {
+    path: __dirname + '/dist/',
+    filename: './js/canvas.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['dist'] },
+      files: ['./dist/*'],
+      notify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    })
+  ],
+  watch: true,
+  devtool: 'source-map'
 }
