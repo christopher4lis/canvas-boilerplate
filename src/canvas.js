27d8e3f39589378cd2cd66a4f589aa28634b2e5f
Gian Gonzaga <40281944+GianFrancescoGonzaga@users.noreply.github.com>
2019-04-01 07:23:49 +0800

    Update constructor functions to es6 classes

--- src/canvas.js
+++ src/canvas.js
@@ -27,23 +27,25 @@ addEventListener('resize', () => {
 })
 
 // Objects
-function Object(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-}
-
-Object.prototype.draw = function() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-}
-
-Object.prototype.update = function() {
-    this.draw()
+class Object {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+    
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+        c.closePath()
+    }
+    
+    update() {
+        this.draw()
+    }
 }
 
 // Implementation
