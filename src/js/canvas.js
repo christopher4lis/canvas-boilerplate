8bee34ac38360a262252154164a9f95bb27d7096
christopher4lis <christopher4lis@gmail.com>
2020-04-02 23:31:30 -0400

    end ep2.

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,11 +1,41 @@
+import gsap from 'gsap'
 import Dot from './Dot'
+
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 const img = document.querySelector('img')
+const radius = 300
+let isImage = true
 
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+function animateDot(dot, canvas) {
+  const rand = Math.random() * Math.PI * 2
+
+  let x = Math.sin(rand) * radius + canvas.width / 2
+  let y = Math.cos(rand) * radius + canvas.height / 2
+
+  if (isImage) {
+    x = dot.imageX
+    y = dot.imageY
+  }
+
+  gsap.to(dot, {
+    duration: 1.75 + Math.random(),
+    x,
+    y,
+    ease: 'cubic.inOut',
+    onComplete: () => {
+      animateDot(dot, canvas)
+    }
+  })
+}
+
+addEventListener('click', () => {
+  isImage = !isImage
+})
 
 addEventListener('load', e => {
   c.drawImage(img, 0, 0)
@@ -32,17 +62,26 @@ addEventListener('load', e => {
     }
   }
 
-  pixels.forEach(pixel => {
-    dots.push(new Dot(pixel.x, pixel.y, pixel.r, pixel.g, pixel.b, 0, 0))
-  })
+  pixels.forEach((pixel, i) => {
+    const imageX = pixel.x + canvas.width / 2 - img.naturalWidth / 2
+    const imageY = pixel.y + canvas.height / 2 - img.naturalHeight / 2
 
-  c.clearRect(0, 0, innerHeight, innerWidth)
+    const rand = Math.random() * Math.PI * 2
+    const x = Math.sin(rand) * radius + canvas.width / 2
+    const y = Math.cos(rand) * radius + canvas.height / 2
+
+    dots.push(new Dot(x, y, pixel.r, pixel.g, pixel.b, imageX, imageY))
+
+    animateDot(dots[i], canvas)
+  })
 
   function animate() {
     requestAnimationFrame(animate)
+    c.clearRect(0, 0, innerWidth, innerHeight)
+
     dots.forEach(dot => {
       dot.draw(c)
-      dot.x++
+      // dot.x++
     })
   }
 
